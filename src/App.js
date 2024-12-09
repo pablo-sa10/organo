800f0907d2ee0c4d97458b889156200230b57f6f
pablo-sa10 <pablomoura2016@gmail.com>
2024-12-09 10:23:01 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Banner from './components/banner/banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/images/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
+export default Banner;
